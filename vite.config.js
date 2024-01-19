94f4115b1e22d77fa8bf7d9d012ccda560f49752
Engineer Adeyemi <66875174+Unclesam112@users.noreply.github.com>
2024-01-19 10:46:42 -0800

    new

--- vite.config.js
+++ vite.config.js
@@ -10,16 +10,13 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      
-      '@': fileURLToPath(new URL('./src', import.meta.url));
-      '@iconify/vue': '@iconify/vue/dist/vue.mjs', // Adjust the path based on your project structure
-    },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
 
-  build: {
-    rollupOptions: {
-      external: ['axios']
-    }
-  }
+  // build: {
+  //   rollupOptions: {
+  //     external: ['axios']
+  //   }
+  // }
 })
